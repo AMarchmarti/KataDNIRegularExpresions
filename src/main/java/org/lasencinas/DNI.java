d3913d68e36d59e0464ae3908c146fd416c78583
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-07 21:47:12 +0200

    Run correctly

--- src/main/java/org/lasencinas/DNI.java
+++ src/main/java/org/lasencinas/DNI.java
@@ -1,7 +1,37 @@
 package org.lasencinas;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class DNI{
 
     //Attributes
-    private String R
+    private Pattern pattern = null;
+    private Matcher match = null;
+
+    //Setters
+
+    public void setPattern(String regex) {
+        this.pattern.compile(regex);
+    }
+
+    public void setMatch(String dni) {
+        this.match = getPattern().matcher(dni);
+    }
+
+    //Getters
+
+    public Pattern getPattern() {
+        return pattern;
+    }
+
+    public Matcher getMatch() {
+        return match;
+    }
+
+    //Logic
+
+    public Boolean verify(String dni, String regex){
+        return dni.matches(regex);
+    }
 }
--- src/test/java/org/lasencinas/DNITest.java
+++ src/test/java/org/lasencinas/DNITest.java
@@ -5,23 +5,19 @@ import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class DNITest
 {
+    private static final String REGEXDNI = "^[0-9]{8}[^IÑU]";
+    private static final String REGEXNIE = "^[XYZ][0-9]{7}[^IÑU]";
     private String [] dnis = {"78484464T","72376173A","01817200Q","95882054E","63587725Q",
                             "26861694V","21616083Q","26868974Y","40135330P","89044648X"};
-
-    @BeforeClass
-    public static void creationTest(){
-        DNI dni = new DNI();
-    }
     @Test
     public void verifyDNITest()
     {
+        DNI dni = new DNI();
         for (String document : dnis){
-            assertTrue(dni.verify(document));
+            assertTrue(dni.verify(document, REGEXDNI));
         }
 
     }
