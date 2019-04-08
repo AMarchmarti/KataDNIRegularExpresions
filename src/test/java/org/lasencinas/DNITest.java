417251a3ed487b6475a8cb13f4370305fb6a6805
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-08 12:47:38 +0200

    Upgrade the regular expresion

--- src/test/java/org/lasencinas/DNITest.java
+++ src/test/java/org/lasencinas/DNITest.java
@@ -9,8 +9,8 @@ import org.junit.Test;
 
 public class DNITest
 {
-    private static final String REGEXDNI = "^[0-9]{8}[^IÑU]";
-    private static final String REGEXNIE = "^[XYZ][0-9]{7}[^IÑU]";
+    private static final String REGEXDNI = "^[0-9]{8}[^IÑOU]";
+    private static final String REGEXNIE = "^[XYZ][0-9]{7}[^IÑOU]";
 
     private String [] dnis = {"78484464T","72376173A","01817200Q","95882054E","63587725Q",
                             "26861694V","21616083Q","26868974Y","40135330P","89044648X"};
