33cbaff1f62a42e891f0ae3b8535caea11e6c271
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-08 13:37:54 +0200

    New test cases, run correctly

--- src/test/java/org/lasencinas/DocumentsTest.java
+++ src/test/java/org/lasencinas/DocumentsTest.java
@@ -14,6 +14,9 @@ public class DocumentsTest
     private String[] nies = {"Y6373752Y", "X2356162L", "Z8259178K", "X0565107C", "X6901377C", "Z7150522B","X1486138Q",
                                 "X9399096P", "Y5060398K", "X3731561H"};
 
+    private String[] cifs = {"R8771524I", "H39679857", "C9163827J", "A95894291", "G1925194A", "N9427139B", "U0432547H",
+            "J2385481C", "N7286889F", "P3429275E"};
+
     private String[] falseDNI = {"98@45184M", "3577820077Y", "8477FP439A", "57333718Ñ", "479543//1B", "9199!!!3373C",
                                     "265R", "P5349239R", "3()335275P", "X4349478G"};
 
@@ -50,6 +53,14 @@ public class DocumentsTest
 
         for (String document : falseNies){
             assertFalse(documents.verify(document, Regex.NIE.getRegex()));
+        }
+    }
+
+    @Test
+    public void verifyCIFTest(){
+        Documents documents = new Documents();
+        for (String document : cifs){
+            assertTrue(documents.verify(document,Regex.CIF.getRegex()));
         }
     }
 
