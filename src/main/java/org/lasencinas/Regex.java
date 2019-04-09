711ab72c1491eda5bef1bafb9a6a8eb79e626a2d
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-04-09 12:49:34 +0200

    Creted new regex to facilitate the exercise

--- src/main/java/org/lasencinas/Regex.java
+++ src/main/java/org/lasencinas/Regex.java
@@ -5,7 +5,10 @@ public enum Regex {
     DNI("^[0-9]{8}[^IÑOU]"),
     NIE("^[XYZ][0-9]{7}[^IÑOU]"),
     CIF("^[A-IJNP-SU-W]([0-9]{7})([0-9]|[A-Z])$"),
-    DNINUM("^[0-9]{8}");
+    DNINUM("^[0-9]{8}"),
+    DNINIELETTER("[^IÑOU]$"),
+    NIEFIRSTLETTER("^[XYZ]"),
+    NIENUM("[0-9]{7}");
 
     private String regex;
 
